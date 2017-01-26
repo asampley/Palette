62a669cec8e0bcd9e304a20fa2960f5ed8c33f2b
asampley <sampley@ualberta.ca>
2017-01-26 14:55:36 -0700

    Player colors now chosen from list by order of connection

--- Assets/Network/NetworkPlayerManager.cs
+++ Assets/Network/NetworkPlayerManager.cs
@@ -5,24 +5,19 @@ using UnityEngine.Networking;
 using UnityEngine.Networking.NetworkSystem;
 
 public class NetworkPlayerManager : NetworkManager {
-	public PaletteColor playerColor = PaletteColor.BLUE;
+	public PaletteColorID[] playerColors = { PaletteColorID.RED, PaletteColorID.BLUE, PaletteColorID.GREEN };
 
-	public override void OnClientConnect (NetworkConnection conn) {
-		IntegerMessage message = new IntegerMessage (PaletteColor.RandomColor().ToInt());
-
-		ClientScene.AddPlayer (conn, 0, message);
-	}
-
-	public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerID, NetworkReader extraMessageReader) {
-
-		var stream = extraMessageReader.ReadMessage<IntegerMessage> ();
-		playerColor = PaletteColor.FromInt(stream.value);
+	public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerID) {
+		PaletteColorID playerColorID = PaletteColorID.WHITE;
+		if (this.numPlayers < playerColors.Length) {
+			playerColorID = playerColors[this.numPlayers];
+		}
 
 		var player = Instantiate (playerPrefab, Vector3.zero, Quaternion.identity);
 		Player pScript = player.GetComponent<Player> ();
-		pScript.colorID = playerColor.ToID();
+		pScript.colorID = playerColorID;
 
-		//Debug.Log ("Created player with color " + playerColor.ToInt());
+		Debug.Log ("Created player with color " + playerColorID);
 
 		NetworkServer.AddPlayerForConnection (conn, player, playerControllerID);
 	}
