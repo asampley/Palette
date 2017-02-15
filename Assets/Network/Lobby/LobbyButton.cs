f2e1a53acb899156faf8d054c53ff8cc1d3caf91
asampley <sampley@ualberta.ca>
2017-02-15 10:55:10 -0700

    Added color to lobby buttons

---
+++ Assets/Network/Lobby/LobbyButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyButton : MonoBehaviour {
+	public int playerNum;
+
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<Image>().color = new PaletteColor(SceneData.sceneObject.GetComponent<PlayerSpawn> ().GetPlayerColorID (playerNum)).ToColor();
+	}
+}
