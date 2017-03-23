d95963d4314270875a1290153626d2e3326105fd
asampley <sampley@ualberta.ca>
2017-03-22 20:01:04 -0600

    Added parallax again

--- Assets/Environment/Parallax.cs
+++ Assets/Environment/Parallax.cs
@@ -2,12 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 [RequireComponent(typeof(SpriteRenderer))]
 public class Parallax : MonoBehaviour {
 	public float parallaxFactor = 1f;
 
 	void Update() {
 		Vector3 localPos = transform.localPosition;
-		localPos.x = (parallaxFactor * transform.position.x) % GetComponent<SpriteRenderer> ().bounds.size.x;
+		Vector3 center = Vector3.zero;
+		center.x = -GetComponent<SpriteRenderer> ().bounds.size.x / 4;
+		center.y = GetComponent<SpriteRenderer> ().bounds.size.y / 2;
+		localPos.x = center.x - Mod(parallaxFactor * transform.parent.position.x, GetComponent<SpriteRenderer> ().bounds.size.x / 2);
+		localPos.y = center.y;
+		transform.localPosition = localPos;
+	}
+
+	float Mod(float a, float b) {
+		return a - b * Mathf.Floor (a / b);
 	}
 }
