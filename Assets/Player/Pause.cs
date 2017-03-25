dd736ffd8f543c9491481563805a32d68a6a3eaa
splant10 <splant.10@gmail.com>
2017-03-25 17:10:26 -0600

    press P to pause now (can pause on character select, and head still moves in pause)

---
+++ Assets/Player/Pause.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+
+	private Canvas UI;
+
+	// Use this for initialization
+	void Start () {
+		UI = this.gameObject.GetComponent<Canvas> ();
+		//player = GameObject.Find ("Player 0");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKeyDown( KeyCode.P ))
+		{
+			TogglePauseMenu ();	
+		}
+	}
+
+	public void TogglePauseMenu()
+	{
+		// not the optimal way but for the sake of readability
+		if (UI.enabled)
+		{
+			UI.enabled = false;
+			Time.timeScale = 1.0f;
+		}
+		else
+		{
+			UI.enabled = true;
+			Time.timeScale = 0f;
+		}
+
+		Debug.Log("TimeScale: " + Time.timeScale);
+	}
+}
