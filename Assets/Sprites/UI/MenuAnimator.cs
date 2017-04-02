a9504fed7dd67289cd93d9223c4d12fad5ada03b
splant10 <splant.10@gmail.com>
2017-04-01 21:42:35 -0600

    more menu changes

--- Assets/Sprites/UI/MenuAnimator.cs
+++ Assets/Sprites/UI/MenuAnimator.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Net;
 
 public class MenuAnimator : MonoBehaviour {
 
     public List<GameObject> uiElements;
     public List<GameObject> uiTargets;
+    private List<Vector3> startPoints;
+
     public float resistance = 15;
     private float speed;
 
@@ -13,22 +17,42 @@ public class MenuAnimator : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
+        startPoints = new List<Vector3>();
+        foreach (GameObject thing in uiElements)
+        {
+            startPoints.Add(thing.transform.position);
+        }
 
+        TestGetIP();
 	}
 	
 	// decelerate as approaching target
 	void Update () {
-        //float step = Mathf.Max(Mathf.Sin(Time.time), Mathf.Cos(3.14f/2)) ;
-        //accel = Mathf.Sin(Time.time)*quickness;
-
-
         for (int i = 0; i < uiElements.Count; ++i)
         {
             speed = Vector3.Distance(uiElements[i].transform.position, uiTargets[i].transform.position)/resistance;
             uiElements[i].transform.position = Vector3.MoveTowards(uiElements[i].transform.position, uiTargets[i].transform.position, speed);
         }
-        
-
     }
-    
+
+    public void ResetAnimation()
+    {
+        try {
+            for (int i = 0; i < uiElements.Count; ++i)
+            {
+                uiElements[i].transform.position = startPoints[i];
+            }
+        } catch (Exception e)
+        {
+            // do nothing
+        }
+    }
+
+    public void TestGetIP()
+    {
+        string strHostName = Dns.GetHostName();
+        IPHostEntry ipEntry = Dns.GetHostEntry(strHostName);
+        IPAddress[] addr = ipEntry.AddressList;
+        Debug.Log(addr[addr.Length - 1].ToString());
+    }
 }
