ce84a72b3453e4691c43be6feb536340741deb62
preyansh <preyansh@ualberta.ca>
2017-04-01 19:59:51 -0600

    wowzers it moves like I imagined

--- Assets/Sprites/UI/MenuAnimator.cs
+++ Assets/Sprites/UI/MenuAnimator.cs
@@ -6,7 +6,8 @@ public class MenuAnimator : MonoBehaviour {
 
     public List<GameObject> uiElements;
     public List<GameObject> uiTargets;
-    public float speed;
+    public float resistance = 15;
+    private float speed;
 
     private Vector3 pos;
 
@@ -15,15 +16,19 @@ public class MenuAnimator : MonoBehaviour {
 
 	}
 	
-	// Update is called once per frame
+	// decelerate as approaching target
 	void Update () {
-        float step = speed * Time.deltaTime;
+        //float step = Mathf.Max(Mathf.Sin(Time.time), Mathf.Cos(3.14f/2)) ;
+        //accel = Mathf.Sin(Time.time)*quickness;
+
 
         for (int i = 0; i < uiElements.Count; ++i)
         {
-            uiElements[i].transform.position = Vector3.MoveTowards(uiElements[i].transform.position, uiTargets[i].transform.position, step);
+            speed = Vector3.Distance(uiElements[i].transform.position, uiTargets[i].transform.position)/resistance;
+            uiElements[i].transform.position = Vector3.MoveTowards(uiElements[i].transform.position, uiTargets[i].transform.position, speed);
         }
         
-    }
 
+    }
+    
 }
