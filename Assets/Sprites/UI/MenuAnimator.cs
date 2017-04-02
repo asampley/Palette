fa40d6840174380f68c7138898b956c160a4fc1e
preyansh <preyansh@ualberta.ca>
2017-04-01 19:08:33 -0600

    checkpoint

--- Assets/Sprites/UI/MenuAnimator.cs
+++ Assets/Sprites/UI/MenuAnimator.cs
@@ -7,25 +7,23 @@ public class MenuAnimator : MonoBehaviour {
     public List<GameObject> uiElements;
     public List<GameObject> uiTargets;
 
-    private List<RectTransform> uiStartLocs;
+    private List<Vector3> starts = new List<Vector3>();
+
 
 	// Use this for initialization
 	void Start () {
-        
+        foreach (GameObject thing in uiElements)
+        {
+            starts.Add(thing.transform.position);
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
-
-    void MoveElementVertically(GameObject el, int dir)
-    {
-
+        for (int i = 0; i < uiElements.Count; ++i)
+        {
+            uiElements[i].transform.position = starts[i] + new Vector3(Mathf.Sin(Time.time), 0.0f, 0.0f);
+        }
     }
 
-    void MoveElementHorizontally(GameObject el, int dir)
-    {
-
-    }
 }
