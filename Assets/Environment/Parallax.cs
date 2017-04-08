43f248f6ec3f1f114d5f5d110ffbd5036c4fec6c
Ryan <kobitoko@gmail.com>
2017-04-08 10:16:35 -0600

    BGImg now twice as long so wont run out except when running too far ahead(for backBG only tho), compressed the pngs with pngyu, and adjusted parallax script to match the width changes. Gotta figure out what to do height wise for camera zoom out.

--- Assets/Environment/Parallax.cs
+++ Assets/Environment/Parallax.cs
@@ -11,9 +11,9 @@ public class Parallax : MonoBehaviour {
 	void Update() {
 		Vector3 localPos = transform.localPosition;
 		Vector3 center = Vector3.zero;
-		center.x = -GetComponent<SpriteRenderer> ().bounds.size.x / 4;
+		center.x = -GetComponent<SpriteRenderer> ().bounds.size.x / 6;
 		center.y = GetComponent<SpriteRenderer> ().bounds.size.y / 2;
-		localPos.x = center.x - Mod(parallaxFactor * transform.parent.position.x, GetComponent<SpriteRenderer> ().bounds.size.x / 2);
+		localPos.x = center.x - Mod(parallaxFactor * transform.parent.position.x, GetComponent<SpriteRenderer> ().bounds.size.x / 3);
 		localPos.y = center.y;
 		localPos.z = -transform.parent.position.z + globalZ;// always set to have global position to avoid moving with the camera
 		transform.localPosition = localPos;
