1293156b218f79c525d1fef2c84f2d17f712f15c
Ryan <kobitoko@gmail.com>
2017-04-08 17:51:46 -0600

    Lobby button grays out when other player already plays as that player.

--- Assets/Network/Lobby/LobbyButton.cs
+++ Assets/Network/Lobby/LobbyButton.cs
@@ -10,4 +10,17 @@ public class LobbyButton : MonoBehaviour {
 	void Start () {
 		this.GetComponent<Image>().color = new PaletteColor(SceneData.sceneObject.GetComponent<PlayerSpawn> ().GetPlayerColorID (playerNum)).ToColor();
 	}
+    private void Update()
+    {
+        Dictionary<int, Player> allPlayers = SceneData.sceneObject.GetComponent<LocalPlayer>().GetPlayers();
+        foreach (KeyValuePair<int, Player> player in allPlayers)
+        {
+            if (player.Key == playerNum)
+            {
+                this.GetComponent<Button>().enabled = false;
+                this.GetComponent<Image>().color = new Color(0.4f, 0.4f, 0.4f);
+            }
+        } 
+        
+    }
 }
