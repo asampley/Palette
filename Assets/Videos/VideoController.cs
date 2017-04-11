d93fe7cec5acaef3dc2956191fa50636da0155c6
asampley <sampley@ualberta.ca>
2017-04-10 18:21:52 -0600

    Roughly added intro cutscene and outro cutscene, but need to fix player connections at start

--- Assets/Videos/VideoController.cs
+++ Assets/Videos/VideoController.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class VideoController : MonoBehaviour {
+	MovieTexture movie;
+	public bool jumpOnMovieFinish;
+	public string nextSceneName;
 
 	// Use this for initialization
 	void Start () {
-        Renderer r = GetComponent<Renderer>();
-        MovieTexture movie = (MovieTexture)r.material.mainTexture;
+        GUITexture g = GetComponent<GUITexture>();
+        movie = (MovieTexture)g.texture;
         movie.Play();
+
+		if (jumpOnMovieFinish) {
+			StartCoroutine(JumpToNextSceneAfterMovie ());
+		}
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	IEnumerator JumpToNextSceneAfterMovie() {
+		yield return new WaitWhile (() => movie.isPlaying);
+
+		if (Network.isServer) {
+			NetworkPlayerManager.singleton.ServerChangeScene (nextSceneName);
+		} else {
+			SceneManager.LoadScene (nextSceneName);
+		}
 	}
 }
